f798077ff8feb539a26c87b1ec1193bf942b6c56
kevinhylant <kevin.hylant@gmail.com>
2015-01-11 17:06:32 -0500

    reading list up and running but not persisting data

--- app/js/app.js
+++ app/js/app.js
@@ -12,7 +12,10 @@
     return {
       restrict: 'E',
       templateUrl: 'partials/book-genres.html',
-      replace: true
+      replace: true,
+      scope: {
+        genres: '='
+      }
     };
   });
 
@@ -31,12 +34,22 @@
       replace: true,
       controller: function(){
         this.showForm = false;
+        this.book = {genres:{}};
+        this.addReview = function(form){
+          books.push(this.book);
+          this.book = {genres:{}};
+          form.$setPristine();
+        };
       },
-      controllerAs: 'reviewFormCtrl'
+      controllerAs: 'reviewFormCtrl',
+      scope: {
+        books: '=',
+        genres: '='
+      }
     };
   });
 
-  var genres = [ 'fable', 'fantasy', 'fiction', 'folklore', 'horror', 'humor', 'legend', 'metafiction', 'mystery', 'mythology', 'non-fiction', 'poetry' ];
+  var genres = [ 'programming', 'javascript', 'ruby', 'php','python','rails','jquery','angular','node' ];
 
   var books = [
     {
@@ -44,27 +57,6 @@
       author: 'George R.R. Martin',
       isbn: '0553593714',
       review: 'The most inventive and entertaining fantasy saga of our time—warrants one hell of an introduction. I loved this book!',
-      rating: 4,
-      genres: { 'non-fiction': true, fantasy: true }
-    },{
-      title: 'HTML for Babies',
-      author: 'John C Vanden-Heuvel Sr',
-      isbn: '0615487661',
-      review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
-      rating: 5,
-      genres: { fiction: true }
-    },{
-      title: 'A is for Array',
-      author: 'Brandon J Hansen',
-      isbn: '1489522212',
-      review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
-      rating: 4,
-      genres: { fiction: true }
-    },{
-      title: 'The Dragon Reborn',
-      author: 'Robert Jordan',
-      isbn: '0812513711',
-      review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
       rating: 4,
       genres: { 'non-fiction': true, fantasy: true }
     }
